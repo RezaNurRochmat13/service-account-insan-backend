d08bb288615e1f2f4afea75b4889c2ee8e50e18a
Reja <rezanurrochmat@gmail.com>
2025-01-09 09:35:09 +0700

    feat(fiber): setup go fiber

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Start a new Fiber App
+	app := fiber.New()
+
+
+	// Send string back for GET calls to the endpoint '/'
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("API is up and running")
+	})
+
+	// Listen on port 3000
+	app.Listen(":3000")
 }
